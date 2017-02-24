b6a88ca6d9b3941abe7db350a04b8af5c8b0bdba
Novotný Rastislav (ERNI) <Rastislav.Novotny@erni.sk>
2017-02-24 14:22:05 +0100

    Initial ASP.NET Core project of authentication service

---
+++ service/ERNI.UnlockIoT.Service/Controllers/FacesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ProjectOxford.Face;
+
+namespace ERNI.UnlockIoT.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class FacesController : Controller
+    {
+        private const string GroupId = "EPD2017ESK";
+
+        private readonly FaceServiceClient faceServiceClient;
+
+        public FacesController(FaceServiceClient faceClient)
+        {
+            faceServiceClient = faceClient;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody]string name)
+        {
+            var groupId = await GetPersonGroupAsync();
+            var person = await faceServiceClient.CreatePersonAsync(groupId, name);
+        }
+
+        private async Task<string> GetPersonGroupAsync()
+        {
+            try
+            {
+                var group = await faceServiceClient.GetPersonGroupAsync(GroupId);
+                return group.PersonGroupId;
+            }
+            catch
+            {
+                await faceServiceClient.CreatePersonGroupAsync(GroupId, "ESK EPD 2017");
+                return GroupId;
+            }
+        }
+    }
+}
