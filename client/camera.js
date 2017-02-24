a0a62fe55e00f0dd66b02175052dd11c71506e17
jakub-sturc <jakub.sturc@erni.sk>
2017-02-24 17:05:39 +0100

    replace v4l2 with fswebcam

--- client/camera.js
+++ client/camera.js
@@ -1,15 +1,6 @@
-var v4l2camera = require("v4l2camera");
+var exec = require('child_process').exec;
 
 module.exports.takeImage = function(file, onSuccess) {
-    var cam = new v4l2camera.Camera("/dev/video0");
-    if (cam.configGet().formatName !== "MJPG") {
-        console.log("NOTICE: MJPG camera required");
-        process.exit(1);
-    }
-    cam.start();
-    cam.capture(function (success) {
-        var frame = cam.frameRaw();
-        require("fs").createWriteStream(file).end(Buffer(frame));
-        cam.stop();
-    });   
+    var cmd = 'fswebcam ' + file;
+    exec(onSuccess);   
 };
