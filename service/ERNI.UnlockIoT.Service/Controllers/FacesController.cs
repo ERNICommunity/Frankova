5ac212cd03df297bdb79662d3c7ffd84dfee8771
Novotný Rastislav (ERNI) <Rastislav.Novotny@erni.sk>
2017-02-24 16:37:49 +0100

    Authenticate service

--- service/ERNI.UnlockIoT.Service/Controllers/FacesController.cs
+++ service/ERNI.UnlockIoT.Service/Controllers/FacesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
 
 namespace ERNI.UnlockIoT.Service.Controllers
 {
@@ -20,8 +21,37 @@ namespace ERNI.UnlockIoT.Service.Controllers
             faceServiceClient = faceClient;
         }
 
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync()
+        {
+            var file = Request.Form.Files[0];
+            using (var image = file.OpenReadStream())
+            {
+                var faces = await faceServiceClient.DetectAsync(image);
+                var faceIds = faces.Select(f => f.FaceId).ToArray();
+                var result = await faceServiceClient.IdentifyAsync(GroupId, faceIds);
+
+                Candidate candidate = null;
+                foreach (var c in result.SelectMany(i => i.Candidates))
+                {
+                    if (candidate == null || candidate.Confidence < c.Confidence)
+                    {
+                        candidate = c;
+                    }
+                }
+
+                if (candidate != null)
+                {
+                    var person = await faceServiceClient.GetPersonAsync(GroupId, candidate.PersonId);
+                    return Ok(person.Name);
+                }
+            }
+
+            return NotFound();
+        }
+
         [HttpPost("add")]
-        public async Task<IActionResult> AddPerson()
+        public async Task<IActionResult> AddPersonAsync()
         {
             var form = await Request.ReadFormAsync();
             var name = form["name"];
